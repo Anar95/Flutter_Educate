e82d7af6dbaa9656d5ad8b0bd91ddfa0a1034d54
Anar95 <anarabbas458@gmail.com>
2023-03-30 10:09:41 +0300

    Text Örnekler

---
+++ flutter_educate_101/android/app/src/main/kotlin/com/example/flutter_educate_101/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_educate_101
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_educate_101/lib/text_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TextView extends StatelessWidget {
+  const TextView({super.key});
+
+  final String name = 'Educate';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Flutter $name ${name.length}  ',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.right,
+              style: ProjectStyle.Style1,
+            ),
+            Text(
+              'Flutter',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.right,
+              style: ProjectStyle.Style1,
+              ),
+          
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProjectStyle {
+  static TextStyle Style1 = const TextStyle(
+    wordSpacing: 2,
+    decoration: TextDecoration.underline,
+    fontStyle: FontStyle.italic,
+    letterSpacing: 2,
+    color: Colors.purple,
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+}
